b318418b715685500fe22d0a2caf383f745e5686
crazyzhou <969677093@qq.com>
2015-06-03 18:15:48 +0800

    DES
    DES

---
+++ DES_12307130088/Form1.Designer.cs
@@ -0,0 +1,243 @@
+﻿namespace DES_12307130088
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(430, 28);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(177, 44);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "打开待加密/解密文件";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(114, 149);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(493, 23);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 152);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "主密钥KEY：";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(626, 152);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(15, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(28, 215);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "初始向量IV：";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(869, 144);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(99, 33);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "主密钥确认";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(869, 207);
+            this.button3.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(99, 33);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "初始向量确认";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(114, 212);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(493, 23);
+            this.textBox2.TabIndex = 7;
+            this.textBox2.Text = "0000000000000000000000000000000000000000000000000000000000000000";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(626, 215);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(22, 17);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "64";
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("微软雅黑", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button4.Location = new System.Drawing.Point(252, 296);
+            this.button4.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(117, 57);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "DES加密";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("微软雅黑", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button5.Location = new System.Drawing.Point(639, 296);
+            this.button5.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(117, 57);
+            this.button5.TabIndex = 10;
+            this.button5.Text = "DES解密";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.ForeColor = System.Drawing.Color.Red;
+            this.label6.Location = new System.Drawing.Point(111, 104);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(68, 17);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "文件未读取";
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(743, 144);
+            this.button6.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(106, 33);
+            this.button6.TabIndex = 13;
+            this.button6.Text = "随机生成KEY";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(743, 207);
+            this.button7.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(106, 33);
+            this.button7.TabIndex = 14;
+            this.button7.Text = "随机生成IV";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(999, 406);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.Text = "Data Encryption Standard By Zhou DES加密算法 ";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ DES_12307130088/Form1.cs
@@ -0,0 +1,317 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DES_12307130088
+{
+    public partial class Form1 : Form
+    {
+        string content;
+        string result;
+        string dir;
+        byte[] KEY = new byte[64];
+        byte[] IV = new byte[64];
+        bool File_Confirm, IV_Confirm, KEY_Confirm;
+        DES new_des = new DES();
+
+        public Form1()
+        {
+            InitializeComponent();
+            File_Confirm = IV_Confirm = KEY_Confirm = false;    //文件已加载、IV已加载、KEY已加载三个标志初始化为false
+        }
+
+        //binCheck函数功能：检验64位字符串是否为二进制字符串，是则返回true，否则返回false
+        bool binCheck(string s)
+        {
+            int i;
+            int len = s.Length;
+            for (i = 0; i < len; i++)
+            {
+                if (s[i] != '0' && s[i] != '1')
+                    return false;
+            }
+            return true;
+        }
+
+        /*
+         * Padding0函数功能：按zeros方法填充待加密串，即全填0直至长度为64倍数，已换用更好的Padding机制
+         * string Padding0(string s)
+         * {
+         * int i;
+         * int len = s.Length;
+         * if (len % 64 == 0)
+         * return s;
+         * else
+         * {
+         * int pd = ((int)(s.Length / 64) + 1) * 64 - s.Length;
+         * for (i = 0; i < pd; i++)
+         * {
+         * s += '0';
+         * }
+         * }
+         * return s;
+         * }
+         */
+
+        //Padding_PKCS5函数功能：按照PKCS#5标准方法进行待加密串填充。
+        //PKCS#5标准方法：填充相同的字节，且该字节的值就是要填充的字节数，恰好8个字节需补8个字节的0x08
+        string Padding_PKCS5(string s)
+        {
+            int i, pd, j;
+            int len = s.Length;
+            pd = 8 - (int)((s.Length % 64) / 8);
+            string pdstr = Convert.ToString(pd, 2);
+            for (i = 0; i < pd; i++) 
+            {
+                for (j = 0; j < 8 - pdstr.Length; j++)
+                {
+                    s += '0';
+                }
+                s += pdstr;
+            }
+            return s;
+        }
+
+        //Depadding_PKCS5函数功能：按照PKCS#5标准方法将已解密串进行去填充化。
+        string Depadding_PKCS5(string s)
+        {
+            int i, pd = 0;
+            string str = s.Substring(s.Length - 8);
+            for (i = 0; i < 8; i++)
+            {
+                pd = (pd << 1) + ((str[i]-'0') & 1);
+            }
+            if (pd > 8)
+            {
+                MessageBox.Show("待解密文件、主密钥或初始向量有误，无法进行正确解密，请检查后重新加载!");
+                content = "";
+                File_Confirm = KEY_Confirm = IV_Confirm = false;
+                label6.ForeColor = Color.Red;
+                label6.Text = "文件未读取";
+                return "";
+            }
+            return s.Substring(0, s.Length - pd * 8);
+        }
+
+        //Random64函数功能：生成随机的64位二进制字符串
+        string Random64()
+        {
+            int i;
+            string s="";
+            Random rnd = new Random();
+            for (i = 0; i < 64; i++)
+            {
+                s += rnd.Next(2);
+            }
+            return s;
+        }
+        
+        //button1：加载文件按钮
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog file = new OpenFileDialog();
+            file.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            file.InitialDirectory = Application.StartupPath;
+            file.ShowReadOnly = true;
+            DialogResult r = file.ShowDialog();
+            if (r == DialogResult.OK)
+            {
+                string filename = file.FileName;
+                dir = System.IO.Path.GetDirectoryName(filename);
+                FileStream aFile = new FileStream(filename, FileMode.Open, FileAccess.Read);
+                StreamReader sr = new StreamReader(aFile);
+                sr.BaseStream.Seek(0, SeekOrigin.Begin);
+                content = sr.ReadToEnd();
+                sr.Close();
+                aFile.Close();
+                if (content.Length % 8 != 0)
+                {
+                    MessageBox.Show("二进制文件未按字节（8 bit）对齐，请进行修改并重新选择文件");
+                    content = "";
+                    return;
+                }
+                if (!binCheck(content))
+                {
+                    MessageBox.Show("文件不符合二进制要求，请进行修改并重新选择文件");
+                    content = "";
+                    return;
+                }
+                File_Confirm = true;
+                label6.ForeColor = Color.Blue;
+                label6.Text = "文件" +filename+"读取成功";
+            }
+        }
+
+        //button2：主密钥确认按钮
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string k = textBox1.Text;
+            if (k.Length != 64 || !binCheck(k)) 
+            {
+                MessageBox.Show("主秘钥不符合规范，应为64位二进制串");
+                return;
+            }
+            for (int i = 0; i < 64; i++)
+            {
+                KEY[i] = (byte)(k[i] - '0');
+            }
+            KEY_Confirm = true;
+            label2.ForeColor = Color.Blue;
+            label2.Text = "主密钥已确认";
+        }
+
+        //button3：初始向量确认按钮
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string ivs = textBox2.Text;
+            if (ivs.Length != 64 || !binCheck(ivs)) 
+            {
+                MessageBox.Show("初始向量不符合规范，应为64位二进制串");
+                return;
+            }
+            for (int i = 0; i < 64; i++)
+            {
+                IV[i] = (byte)(ivs[i] - '0');
+            }
+            IV_Confirm = true;
+            label4.ForeColor = Color.Blue;
+            label4.Text = "初始向量已确认";
+        }
+
+        //button4：逐64bit进行CBC密码分组模式的DES加密，并写入同一目录的文件CRYTOGRAM.txt
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (!File_Confirm)
+            {
+                MessageBox.Show("文件未读取!");
+                return;
+            }
+            if (!KEY_Confirm)
+            {
+                MessageBox.Show("主密钥未确认!");
+                return;
+            }
+            if (!IV_Confirm)
+            {
+                MessageBox.Show("初始向量未确认!");
+                return;
+            }
+            content = Padding_PKCS5(content);
+            result = new_des.DES_Encrypt(KEY, IV, content);
+            if (File.Exists(dir + @"\CRYPTOGRAM.txt")) 
+            {
+                File.Delete(dir+@"\CRYPTOGRAM.txt");
+            }
+            FileStream bFile = new FileStream(dir+@"\CRYPTOGRAM.txt", FileMode.CreateNew);
+            StreamWriter sw = new StreamWriter(bFile);
+            sw.Write(result);
+            sw.Close();
+            bFile.Close();
+            DialogResult dr;
+            dr = MessageBox.Show("加密完成！！", "DES");
+            if (dr == System.Windows.Forms.DialogResult.OK)
+            {
+                File_Confirm = IV_Confirm = KEY_Confirm = false;
+                content = "";
+                label6.ForeColor = Color.Red;
+                label6.Text = "文件未读取";
+                System.Diagnostics.Process.Start("notepad.exe", dir + @"\CRYPTOGRAM.txt");
+            }
+        }
+
+        //button5：逐64bit进行CBC密码分组模式的DES解密，并写入同一目录的文件SOURCE.txt
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (!File_Confirm)
+            {
+                MessageBox.Show("文件未读取!");
+                return;
+            }
+            if (!KEY_Confirm)
+            {
+                MessageBox.Show("主密钥未确认!");
+                return;
+            }
+            if (!IV_Confirm)
+            {
+                MessageBox.Show("初始向量未确认!");
+                return;
+            }
+            if (content.Length % 64 != 0)
+            {
+                MessageBox.Show("待解密文件长度有误，应为64bit的倍数，请检查后重新加载!");
+                content = "";
+                File_Confirm = false;
+                label6.ForeColor = Color.Red;
+                label6.Text = "文件未读取";
+                return;
+            }
+            result = new_des.DES_Decrypt(KEY, IV, content);
+            result = Depadding_PKCS5(result);
+            if (result != "")
+            {
+                if (File.Exists(dir + @"\SOURCE.txt"))
+                {
+                    File.Delete(dir + @"\SOURCE.txt");
+                }
+                FileStream bFile = new FileStream(dir + @"\SOURCE.txt", FileMode.CreateNew);
+                StreamWriter sw = new StreamWriter(bFile);
+                sw.Write(result);
+                sw.Close();
+                bFile.Close();
+                DialogResult dr;
+                dr = MessageBox.Show("解密完成！！", "DES");
+                if (dr == System.Windows.Forms.DialogResult.OK)
+                {
+                    File_Confirm = IV_Confirm = KEY_Confirm = false;
+                    content = "";
+                    label6.ForeColor = Color.Red;
+                    label6.Text = "文件未读取";
+                    System.Diagnostics.Process.Start("notepad.exe", dir + @"\SOURCE.txt");
+                }
+            }
+        }
+
+        //button6：随机生成主密钥KEY
+        private void button6_Click(object sender, EventArgs e)
+        {
+            KEY_Confirm = false;
+            textBox1.Text = Random64();
+        }
+
+        //button7：随机生成初始向量IV
+        private void button7_Click(object sender, EventArgs e)
+        {
+            IV_Confirm = false;
+            textBox2.Text = Random64();
+        }
+        
+        //timer1：时钟，时刻保持label2，label4内容（输入的主密钥长度、初始向量长度）
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (!KEY_Confirm)
+            {
+                if (textBox1.TextLength !=64)
+                    label2.ForeColor = Color.Red;
+                else
+                    label2.ForeColor = Color.Green;
+                label2.Text = textBox1.TextLength.ToString();
+            }
+            if (!IV_Confirm)
+            {
+                if (textBox2.TextLength != 64)
+                    label4.ForeColor = Color.Red;
+                else
+                    label4.ForeColor = Color.Green;
+                label4.Text = textBox2.TextLength.ToString();
+            }
+        }
+    }
+}
